46ecca4a5217b9197499ae408f2d05492dd6535c
Victor <1victorjrortiz3@gmail.com>
2026-04-10 10:36:08 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Properties_Ortiz_Victor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
