afa1b5d06315354fcf270c0903c2d457a8881759
Victor <1victorjrortiz3@gmail.com>
2026-04-13 08:44:45 -0500

    a lot of stuff happened

---
+++ Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Properties_Ortiz_Victor
+{
+    internal class Car
+    {
+        // A field that protects the Model property
+        private string model;
+        // A property that sets and returns a new value that is set when called and changed
+        public string Model { get { return model; } set { model = value; } }
+        // A automatic property that sets the return and set values automatically 
+        public string Make { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creates a object to referance the Car class
+            Car C1 = new Car();
+            // Sets the Make and Model to a new value from the set in the Car class
+            C1.Model = "Mustang";
+            C1.Make = "Ford";
+            // Prints a sentance to the console with the Model and Make values
+            Console.WriteLine($"Car numbero Uno: Make = {C1.Make}, Model = {C1.Model}");
+            // Creates a second separate object to referance to the car class
+            Car C2 = new Car();
+            // Sets the Make and Model to a new value from the set in the Car class
+            C2.Model = "Civic";
+            C2.Make = "Honda";
+            // Prints a sentance to the console with the Model and Make values
+            Console.WriteLine($"Car numbero Dos: Make = {C2.Make}, Model = {C2.Model}");
         }
     }
 }
